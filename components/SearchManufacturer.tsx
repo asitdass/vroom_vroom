e8e6d7a5c0ce620f4b7d8c17283c063fce34803e
Asit Rohan Dass <asitdass33@gmail.com>
2023-07-09 21:51:45 +0530

    components created for searchbar and filter

---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { SearchManufacturerProps } from '@/types';
+const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerProps) => {
+  return (
+    <div>SearchManufacturer</div>
+  )
+}
+
+export default SearchManufacturer;
